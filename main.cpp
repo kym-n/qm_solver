9024c068dc5bcb1b595fe1fa4744cb0708a9b40b
yuya0084 <yuya0084.66th@gmail.com>
2022-06-23 02:48:18 +0900

    min_l option & ex_val output & vecs output

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@ using namespace Eigen;
 #include "params.h"
 SelfAdjointEigenSolver<SparseMatrix<double>> solve(int pot_type, int l, int diff_precision);
 
-int main(int argc, char **argv) // pot_type max_l name (N (diff_precision))
+int main(int argc, char **argv) // pot_type min_l max_l name (N (diff_precision))
 {
     // input
-    int pot_type = atoi(argv[1]), max_l = atoi(argv[2]); // pot_type 0: well, 1: harmo, 2: morse
-    std::string path_name = "results/" + (std::string)argv[3] + "/";
-    if (argc >= 5)
-        N = atoi(argv[4]);
+    int pot_type = atoi(argv[1]);
+    int min_l = atoi(argv[2]), max_l = atoi(argv[3]); // pot_type 0: well, 1: harmo, 2: morse
+    std::string path_name = "results/" + (std::string)argv[4] + "/";
     if (argc >= 6)
-        diff_precision = atoi(argv[5]);
+        N = atoi(argv[5]);
+    if (argc >= 7)
+        diff_precision = atoi(argv[6]);
 
     std::cout << "output path: " << path_name << std::endl;
     std::cout << "length: " << L << std::endl;
@@ -39,19 +40,33 @@ int main(int argc, char **argv) // pot_type max_l name (N (diff_precision))
     {
         std::ofstream out_vals(path_name + "ex_vals.txt");
         out_vals << std::setprecision(10) << std::fixed;
-        for (int i = 0;; i++)
+        switch (pot_type)
         {
-            double num = 0.5 + i;
-            out_vals << om * num - omx * num * num - D_e << std::endl;
-            if (num >= om / (2 * omx))
-                break;
+        case 1:
+            for (int i = 0; i < N; i++)
+            {
+                double num = 0.5 + i;
+                out_vals << om * num << std::endl;
+            }
+            break;
+
+        case 2:
+        default:
+            for (int i = 0;; i++)
+            {
+                double num = 0.5 + i;
+                out_vals << om * num - omx * num * num - D_e << std::endl;
+                if (num >= om / (2.0 * omx))
+                    break;
+            }
+            break;
         }
         out_vals.close();
     }
 
     // solve & output for l = 0...max_l
     clock_t start = clock();
-    for (int l = 0; l <= max_l; l++)
+    for (int l = min_l; l <= max_l; l++)
     {
         std::cout << "solve on l = " << l << "." << std::endl;
         SelfAdjointEigenSolver<SparseMatrix<double>> es = solve(pot_type, l, diff_precision);
@@ -59,7 +74,7 @@ int main(int argc, char **argv) // pot_type max_l name (N (diff_precision))
         std::cout << "writing..." << std::endl;
         std::ofstream out_vals(path_name + std::to_string(l) + "_vals.txt"), out_vecs(path_name + std::to_string(l) + "_vecs.txt");
         out_vals << std::setprecision(10) << std::fixed << es.eigenvalues() << std::endl;
-        out_vecs << std::setprecision(6) << std::scientific << es.eigenvectors() << std::endl;
+        out_vecs << std::setprecision(6) << std::fixed << es.eigenvectors() << std::endl;
         out_vals.close();
         out_vecs.close();
     }
